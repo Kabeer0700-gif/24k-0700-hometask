669eaf633a62ff4be849057a4c7b406389528876
Kabeer0700-gif <kb3463217@gmail.com>
2025-11-29 08:14:54 -0800

    Update q3.cpp

--- LAB 10/q3.cpp
+++ LAB 10/q3.cpp
@@ -1,65 +1,105 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Heap {
+int priority(char c){
+    if(c == 'A') return 5;
+    if(c == 'B') return 3;
+    if(c == 'C') return 8;
+    if(c == 'D') return 6;
+
+}
+class MaxHeap{
 public:
-    int arr[20];
+    char* ch;
     int size;
-    Heap() { size = 0; }
+    int capacity;
 
-    void insert(int data) {
-        size++;
-        int index = size;
-        arr[index] = data;
-        while (index > 1) {
-            int parent = index / 2;
-            if (arr[parent] < arr[index]) {
-                swap(arr[parent], arr[index]);
-                index = parent;
-            } else break;
-        }
+MaxHeap(int cap){
+    capacity = cap + 1;
+    ch = new char[capacity];
+    size = 0;
+}
+
+void insert(char c){
+    if(size == capacity){
+        cout<<"heap Overflow"<<endl;
+        return;
     }
 
-    void removeMax() {
-        if (size == 0) return;
-        arr[1] = arr[size];
-        size--;
+    size++;
+    ch[size] = c;
+    int i = size;
 
-        int index = 1;
-        while (true) {
-            int left = index * 2;
-            int right = index * 2 + 1;
-            int largest = index;
-
-            if (left <= size && arr[left] > arr[largest]) largest = left;
-            if (right <= size && arr[right] > arr[largest]) largest = right;
-
-            if (largest != index) {
-                swap(arr[index], arr[largest]);
-                index = largest;
-            } else break;
-        }
+    while(i!=1 && priority(ch[i]) > priority(ch[i/2])){
+        swap(ch[i],ch[i/2]);
+        i/=2;
     }
 
-    void display() {
-        for (int i = 1; i <= size; i++) cout << arr[i] << " ";
-        cout << endl;
+    return;
+}
+
+char deleteMax(){
+    if(size == 0){
+        cout<<"Heap is empty"<<endl;
+        return -1;
     }
+
+    char root = ch[1];
+    ch[1] = ch[size];
+    size--;
+    heapify(1);
+    return root;
+}
+
+void heapify(int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i + 1;
+
+    if(left <= size && priority(ch[left]) > priority(ch[largest])) largest = left;
+    if(right <= size && priority(ch[right]) > priority(ch[largest])) largest = right;
+
+    if(largest != i){
+        swap(ch[i],ch[largest]);
+        heapify(largest);
+    }
+
+    return;
+}
+
+
+
+void display(){
+     if(size == 0){
+        cout<<"heap is full"<<endl;
+    }
+
+    for(int i=1; i<=size; i++){
+        cout<<ch[i]<<" ";
+    }
+}
+
+~MaxHeap(){
+    delete[] ch;
+}
 };
 
-int main() {
-    Heap h;
+int main(){
+     MaxHeap heap(5);
 
-    h.insert(5);
-    h.insert(3);
-    h.insert(8);
-    h.display();
+    heap.insert('A');
+    heap.insert('B');
+    heap.insert('C');
 
-    h.removeMax();
-    h.display();
+     cout << "Deleting max: " << heap.deleteMax() << endl;
+    cout << "After delete: \n";
+    heap.display();
 
-    h.insert(6);
-    h.display();
+    heap.insert('D');
+    cout << "\nAfter insert task D: ";
+    heap.display();
+
+  
 
     return 0;
 }
